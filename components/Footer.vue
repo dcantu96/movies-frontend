d596870b4d6b670aa49636b5dd5350e96da40ea4
David Cantu Martinez <davidcantu966@gmail.com>
2019-08-11 18:16:47 -0500

    First commit

---
+++ components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <a-layout-footer :style="{ textAlign: 'center' }">
+    Ant Design 2018 Created by Ant UED
+  </a-layout-footer>
+</template>
---
+++ components/Nav.vue
@@ -0,0 +1,27 @@
+<template>
+  <a-layout-header class="nav">
+    <div class="logo" />
+    <a-menu class="menu" theme="dark" mode="horizontal">
+      <a-menu-item>Movies</a-menu-item>
+      <a-menu-item>Directors</a-menu-item>
+      <a-menu-item>Actors</a-menu-item>
+    </a-menu>
+  </a-layout-header>
+</template>
+<style>
+.nav {
+  position: 'fixed';
+  z-index: 1;
+  width: '100%';
+}
+.menu {
+  line-height: '64px';
+}
+.logo {
+  width: 120px;
+  height: 31px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px 24px 16px 0;
+  float: left;
+}
+</style>
